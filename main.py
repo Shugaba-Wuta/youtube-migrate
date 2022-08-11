4969468eaa88bfe24336bd5ed85a679c1a637938
Shugaba-Wuta <57352948+Shugaba-Wuta@users.noreply.github.com>
2022-08-11 18:39:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import uvicorn
 from dotenv import load_dotenv
 load_dotenv(".env")
 
+
+environ = os.environ.get("ONLINE", False) 
+client_secret = os.environ.get("CLIENT_SECRET", None)
+if environ and client_secret is not None: 
+  with open("client_secret.json", "w") as secret: 
+    secret.write(client_secret)
+
 from frontend.crud import app
 
 
