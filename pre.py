b32e6d581038daf4bd4db791fc5f522f3d910a57
Shugaba-Wuta <57352948+Shugaba-Wuta@users.noreply.github.com>
2022-08-11 18:30:09 +0100

    Update pre.py

--- pre.py
+++ pre.py
@@ -5,5 +5,5 @@ load_dotenv()
 environ = os.environ.get("ONLINE", False) 
 client_secret = os.environ.get("CLIENT_SECRET", None)
 if environ and client_secret is not None: 
-  with open("client_secret.json", w) as secret: 
-    secret.write(client_secret, index=4)
+  with open("client_secret.json", "w") as secret: 
+    secret.write(client_secret)
