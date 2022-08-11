448cc970480b7125e5ae61c6cba9f2a12b4f20d7
Shugaba-Wuta <57352948+Shugaba-Wuta@users.noreply.github.com>
2022-08-11 11:53:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ from frontend.crud import app
 if __name__ == "__main__":
 # A custom way of starting the server.
 # ACtivated by runnning this file as a regular python script
-    uvicorn.run("main:app", reload=True, port=5333)
+    uvicorn.run("main:app")
