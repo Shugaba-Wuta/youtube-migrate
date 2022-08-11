98291a77d0c0956d9b3575d03285ee99e0fa7e98
Shugaba-Wuta <57352948+Shugaba-Wuta@users.noreply.github.com>
2022-08-11 18:24:34 +0100

    Create pre.py
    Sets the client_secret on the cloud

---
+++ pre.py
@@ -0,0 +1,9 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+environ = os.environ.get("ONLINE", False) 
+client_secret = os.environ.get("CLIENT_SECRET", None)
+if environ and client_secret is not None: 
+  with open("client_secret.json", w) as secret: 
+    secret.write(client_secret, index=4)
