67a86beed6ca8d915a81f3a2f830bab0ae3a74bf
Shugaba-Wuta <57352948+Shugaba-Wuta@users.noreply.github.com>
2022-08-12 23:09:12 +0100

    Uvicorn setup

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ from dotenv import load_dotenv
 import os
 load_dotenv(".env")
 
-
-environ = os.environ.get("ONLINE", False) 
-client_secret = os.environ.get("CLIENT_SECRET", None)
-if environ and client_secret is not None: 
-  with open("client_secret.json", "w") as secret: 
-    secret.write(client_secret)
-
+import pre
 from frontend.crud import app
 
 
 
-if __name__ == "__main__":
-# A custom way of starting the server.
-# ACtivated by runnning this file as a regular python script
-    uvicorn.run("main:app")
+# if __name__ == "__main__":
+# # A custom way of starting the server.
+# # ACtivated by runnning this file as a regular python script
+#     uvicorn.run("main:app")
