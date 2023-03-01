75c94dcddafe9d9c0337b40a47384fb8c74680c1
Shugaba Wuta <shugabawuta@gmail.com>
2023-03-01 21:06:58 +0100

    WIP

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(bywuta.com www.bywuta.com)
+domains=(youtube-migrate.bywuta.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="shugabawuta@gmail.com" # Adding a valid address is strongly recommended
